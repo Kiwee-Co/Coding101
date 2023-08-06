e2af66d218ecd52793c5910dc3a2c05a96e5a8da
Jerry Yang <jerry@remiyang.us>
2023-08-06 16:23:21 -0700

    update install shell script

--- 01.Introduction/install.sh
+++ 01.Introduction/install.sh
@@ -1,20 +1,25 @@
 #!/usr/bin/env bash 
 
+# Apple silicon returns arm64; intel mac returns x86_64
+ARCH=`uname -m`
+
 echo "Installing brew..."
 #/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" 
+
 echo "brew installed!"
 
 echo "now installing necessary packages..."
 
-PKGS=(bash guntls bash-completion git btop tree unrar wget curl openjdk@17 maven maven-completion eclipse-java sourcetree iterm2 )
+PKGS=(bash guntls bash-completion git btop tree unrar wget curl openjdk@17 maven maven-completion eclipse-jee sourcetree iterm2 netcat )
 
 SEGMENT=7
 
 
 install() {
-  local -n param1=$1
-  local brew_command=$2
-  local group_size=$3
+
+  local param1=("${!1}") 	# packages
+  local brew_command=$2		# brew cmd
+  local group_size=$3		# size
 
   for ((i = 0; i < ${#param1[@]}; i += group_size)); do
     PKG=""
@@ -22,20 +27,35 @@ install() {
       PKG="$PKG ${param1[j]}"
     done
     echo "  - Installing $PKG"
-    brew install $PKG -q
+    brew install $PKG -q 
   done
 }
 
 
-install PKGS "install" $SEGMENT 
+install PKGS[@] install $SEGMENT 
+
+echo "packages installed!"
 
 # copy git prompt
 cp .bash ~/.bash
-echo "source ~/.bash" >> .bash_profile
+echo "source ~/.bash" >> ~/.bash_profile
 cp .git-prompt.sh ~/
 cp .git-competion.bash ~/
 
-chsh -s /usr/local/bin/bash
+
+# setup git repo
+mkdir -p ~/Projects/kiwee-co/
+cd ~/Projects/kiwee-co/
+git clone https://github.com/Kiwee-Co/Coding101.git
+git clone https://github.com/Kiwee-Co/CodingExercise101.git
+
+echo "setup bash for $ARCH mac..."
+
+if [[ $ARCH == 'arm' ]]; then
+  chsh -s /opt/homebrew/bin/bash
+else
+  chsh -s /usr/local/bin/bash
+fi
 
 brew cleanup -s --prune=all 
 
